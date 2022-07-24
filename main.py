4916596c2f0b915d480c9b724f57e719b62cb173
bushra <bushra.azbm@gmail.com>
2022-07-24 12:09:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+''''''
+
+'''
+# starting commands
+    - py -m pip install django
+    - pip3 install djangorestframework
+    - pip3 install djangorestframework-simplejwt
+    - django-admin startproject Articles
+    - cd Articles
+    - py manage.py startapp ArticlesApp
+    - py manage.py startapp Accounts
+
+'''
