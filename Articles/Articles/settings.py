f4dff8e9df593ee27af201f7967014680c25d513
bushra <bushra.azbm@gmail.com>
2022-07-24 14:56:15 +0300

    delete serialzer.py to pull new edit

--- Articles/Articles/settings.py
+++ Articles/Articles/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounts',
-    'ArticlesApp'
+    'ArticlesApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
