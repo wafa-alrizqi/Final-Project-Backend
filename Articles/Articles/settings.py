d61a8488839897b2d8024b68a2cf21868c2b22ce
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-07-24 16:26:12 +0300

    Edit Setting ACCESS_TOKEN_LIFETIME

--- Articles/Articles/settings.py
+++ Articles/Articles/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +83,9 @@ WSGI_APPLICATION = 'Articles.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
