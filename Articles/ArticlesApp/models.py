6d546f2bd2f2b35f07ea2b53ff9b320d081be85e
bushra <bushra.azbm@gmail.com>
2022-07-24 13:58:31 +0300

    wrote the modles.py

--- Articles/ArticlesApp/models.py
+++ Articles/ArticlesApp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category (models.Model):
+    '''This class for Category attributes '''
+
+    title = models.CharField(max_length=128)
+    image = models.URLField()
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Article (models.Model):
+    ''' This class is for Articles attributes'''
+
+    title = models.CharField(max_length=128)
+    image = models.URLField()
+    content = models.CharField()
+    summary = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    refrence = models.URLField()
+    likes = models.IntegerField()
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.title
+
+
+class Comment (models.Model):
+    ''' This class for Comment attributes '''
+
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class BookMarks (models.Model):
+    ''' This class for BookMarks attributes '''
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
